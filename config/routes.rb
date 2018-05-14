ed82f9ba0cec4d3db280fb712a03bb0643a26c06
Eric Wee <wee007@gmail.com>
2018-05-14 23:11:17 +1000

    Define API route for page view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    # Display sample data
+    get "/page_view", to: "page_view#index"
+
+    post "/page_view", to: "page_view#fetch"
+  end
 end
